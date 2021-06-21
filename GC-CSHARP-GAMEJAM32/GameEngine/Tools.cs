2a1db430864b3e9431c9d17e3406e7e89e49478c
Alain Kelleter <alain.kelleter@kelleter.be>
2021-06-21 15:39:32 +0200

    First Commit

--- GC-CSHARP-GAMEJAM32/GameEngine/Tools.cs
+++ GC-CSHARP-GAMEJAM32/GameEngine/Tools.cs
@@ -11,22 +11,30 @@ namespace AlphaKilo_GameJam32
     class Tools
     {
         static Random randomGen = new Random();
-
         public enum SizeScreenType
         {
             Width,
             Height
         }
+        /**
+         * Retourne un nombre entier compris entre les valeurs min et max passées en paramètre
+         */
         public static int RandomInt(int pMin, int pMax)
         {
             return randomGen.Next(pMin, pMax + 1);
         }
 
+        /**
+         * Initialise la graine() de la fonction Random
+         */
         public static void SetRandomSeed(int pSeed)
         {
             randomGen = new Random(pSeed);
         }
 
+        /**
+         * Retourne en fonction du paramètre la largeyr ou la longeur de la fenêtre du jeu
+         */
         public static int ScreenSize(Rectangle pScreen, SizeScreenType pType)
         {
             int size = 1;
@@ -46,9 +54,28 @@ namespace AlphaKilo_GameJam32
             return size;
         }
 
+        /**
+         * Détection de collision entre deux box
+         */
         public static bool CollideByBox(IActor pBox1, IActor pBox2)
         {
             return pBox1.boundingBox.Intersects(pBox2.boundingBox);
         }
+
+        /**
+         * Retourne la distance entre deux points 
+         */
+        public static double MathDist(double x1, double y1, double x2, double y2)
+        {
+            return Math.Pow(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2), 0.5);
+        }
+        /**
+         * Retourne l'angle entre deux points
+         */
+        public static double MathAngle(double x1, double y1, double x2, double y2)
+        {
+            return Math.Atan2(y2 - y1, x2 - x1);
+        }
     }
 }
+
--- GC-CSHARP-GAMEJAM32/MainGame.cs
+++ GC-CSHARP-GAMEJAM32/MainGame.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace AlphaKilo_GameJam32
 {
